654318ed40b4bbb582565dbce6ab10f3a5d480c5
小野裕輔 <y-ono@nswt.co.jp>
2022-04-10 10:00:59 +0900

    Add 3.3

--- spring-boot1/src/main/java/com/example/demo/controller/HelloController.java
+++ spring-boot1/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,15 +1,26 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String index() {
+    @GetMapping("/")
+    public String greeting() {
 
         // hello,htmlを呼び出す
+        return "hello";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam("message") String message,
+                           Model model) {
+        model.addAttribute("sample",
+                message);
         return "hello";
     }
 }
