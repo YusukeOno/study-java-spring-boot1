c2974644f68dce1ab183ce2a199e4793ac2d8351
小野裕輔 <y-ono@nswt.co.jp>
2022-04-11 10:29:38 +0900

    Add 6.3

---
+++ spring-jpa3/src/main/java/com/example/demo/config/DataLoader.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import com.example.demo.model.Department;
+import com.example.demo.repository.DepartmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class DataLoader implements ApplicationRunner {
+    private final DepartmentRepository repository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        var sales = new Department();
+        sales.setName("営業");
+        repository.save(sales);
+
+        var development = new Department();
+        development.setName("開発");
+        repository.save(development);
+    }
+}
